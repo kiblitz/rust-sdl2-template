eaac04fb25caee64493e90fd9386939055d3bb85
glee <g.lee13770334@gmail.com>
2024-07-05 19:06:21 -0400

    Add drag util helper

--- src/util.rs
+++ src/util.rs
@@ -4,8 +4,13 @@ const NS_IN_S: u32 = 1_000_000_000u32;
 const REFRESH_RATE: u32 = 60;
 pub const REFRESH_EVERY: Duration = Duration::new(0, NS_IN_S / REFRESH_RATE);
 
-#[warn(dead_code)]
+#[inline(always)]
 pub fn lerp(current: f32, target: f32, c: f32, delta_time: f32) -> f32 {
     let percent = 1. - c.powf(delta_time);
-    return current + (target - current) * percent;
+    current + (target - current) * percent
+}
+
+#[inline(always)]
+pub fn drag(current: f32, c: f32, delta_time: f32) -> f32 {
+    current * c.powf(delta_time)
 }
